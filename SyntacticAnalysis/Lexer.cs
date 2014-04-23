1e5b24432230933a16d67b4c6a716ce44e2e89ec
B_head <b_head@outlook.jp>
2014-04-24 00:38:08 +0900

    usingの整理。

--- SyntacticAnalysis/Lexer.cs
+++ SyntacticAnalysis/Lexer.cs
@@ -1,9 +1,5 @@
-﻿using System;
+﻿using AbstractSyntax;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AbstractSyntax;
 
 namespace SyntacticAnalysis
 {
