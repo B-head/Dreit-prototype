f75c54023da0fb26e5b31d5efdc3057639f4d934
B_head <b_head@outlook.jp>
2014-04-24 02:38:43 +0900

    TestCaseお試し。

---
+++ SyntacticAnalysisTest/TokenizerTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using SyntacticAnalysis;
+using System;
+
+namespace SyntacticAnalysisTest
+{
+    [TestFixture]
+    public class TokenizerTest
+    {
+        private Tokenizer empty;
+
+        [SetUp]
+        public void SetUp()
+        {
+            empty = new Tokenizer("", "empty");
+        }
+
+        [TestCase("", 0, false)]
+        [TestCase("abc", 0, true)]
+        [TestCase("abc", 2, true)]
+        [TestCase("abc", 3, false)]
+        public void IsReadable(string text, int index, bool expected)
+        {
+            Tokenizer t = new Tokenizer(text, string.Empty);
+            Assert.That(t.IsReadable(index), Is.EqualTo(expected));
+        }
+    }
+}
