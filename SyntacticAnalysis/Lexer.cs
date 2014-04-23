165f07bb2bb482fc6e52766f14e1963c4391e01d
B_head <b_head@outlook.jp>
2014-04-24 00:33:58 +0900

    不要な関数を削除。

--- SyntacticAnalysis/Lexer.cs
+++ SyntacticAnalysis/Lexer.cs
@@ -10,7 +10,6 @@ namespace SyntacticAnalysis
     public partial class Lexer
     {
         private delegate Token LexerFunction(Tokenizer t);
-        private delegate bool LexerFunctionOld(ref TextPosition p);
         public string Text { get; private set; }
         public string FileName { get; private set; }
         public TextPosition LastPosition { get; private set; }
@@ -76,16 +75,6 @@ namespace SyntacticAnalysis
             return TokenType.OtherString;
         }
 
-        private bool IsEnable(TextPosition p, int i)
-        {
-            return p.Total + i < Text.Length;
-        }
-
-        private char Peek(TextPosition p, int i)
-        {
-            return Text[p.Total + i];
-        }
-
         private Token DisjunctionLexer(Tokenizer t, params LexerFunction[] func)
         {
             foreach (var f in func)
@@ -97,91 +86,6 @@ namespace SyntacticAnalysis
                 }
             }
             return null;
-        }
-
-        private void DisjunctionLexer(ref TextPosition p, params LexerFunctionOld[] func)
-        {
-            foreach (LexerFunctionOld f in func)
-            {
-                if (f(ref p))
-                {
-                    break;
-                }
-            }
-        }
-
-        private bool TakeAddToken(ref TextPosition p, int length, TokenType type)
-        {
-            if (length == 0)
-            {
-                return false;
-            }
-            Token token = TakeToken(ref p, length, type);
-            _Token.Add(token);
-            return true;
-        }
-
-        private bool TakeAddErrorToken(ref TextPosition p, int length, TokenType type)
-        {
-            if (length == 0)
-            {
-                return false;
-            }
-            Token token = TakeToken(ref p, length, type);
-            _ErrorToken.Add(token);
-            return true;
-        }
-
-        private Token TakeToken(ref TextPosition p, int length, TokenType type)
-        {
-            string text = TrySubString(p.Total, length);
-            TextPosition temp = p;
-            temp.Length = length;
-            Token token = new Token { Text = text, Type = type, Position = temp };
-            p.Total += length;
-            p.Row += length;
-            if (type == TokenType.LineTerminator)
-            {
-                p.Line++;
-                p.Row = 0;
-            }
-            return token;
-        }
-
-        private bool SkipToken(ref TextPosition p, int length)
-        {
-            if (length == 0)
-            {
-                return false;
-            }
-            p.Total += length;
-            p.Row += length;
-            return true;
-        }
-
-        private string TrySubString(int startIndex, int length)
-        {
-            return startIndex + length <= Text.Length ? Text.Substring(startIndex, length) : string.Empty;
-        }
-    }
-
-    static class LexerExtension
-    {
-        public static bool Match<V, T>(this V value, IEnumerable<T> list) where V : IEquatable<T>
-        {
-            foreach (T v in list)
-            {
-                if (value.Equals(v))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public static bool Match<V>(this V value, V stert, V end) where V : IComparable<V>
-        {
-            return value.CompareTo(stert) >= 0 && value.CompareTo(end) <= 0;
         }
     }
 }
