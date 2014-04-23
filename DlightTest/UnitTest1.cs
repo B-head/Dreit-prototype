c38b40316bcf719de9d7c1608adbf5c0ec384b36
B_head <b_head@outlook.jp>
2014-04-23 14:27:38 +0900

    NUnitを導入

---
+++ DlightTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DlightTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
