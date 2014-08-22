b1573c1fb9b522f6bbf5768e693d55dc513214ee
B_head <b_head@outlook.jp>
2014-08-22 22:42:17 +0900

    ソースコードにライセンス表記を追加。

--- DreitCompiler/CompileMessageBuilder.cs
+++ DreitCompiler/CompileMessageBuilder.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+Copyright 2014 B_head
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
@@ -16,8 +31,9 @@ namespace Dlight
 
         static CompileMessageBuilder()
         {
+            var directory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
             messageBase = new Dictionary<string, string>();
-            foreach(var file in Directory.EnumerateFiles(@"./", "*.xml"))
+            foreach (var file in Directory.EnumerateFiles(directory, "*.xml"))
             {
                 var element = XElement.Load(file);
                 if(element.Name != ns + "compile-message")
